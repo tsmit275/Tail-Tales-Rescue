8269f7a5fbb4613e715206ca7f57f81346960b69
Melanie Allmond <140862029+mtallmond@users.noreply.github.com>
2024-01-20 10:23:22 -0500

    Update animals.jsx

--- frontend/src/pages/animals.jsx
+++ frontend/src/pages/animals.jsx
@@ -1,5 +1,7 @@
 
 import { useEffect, useState } from 'react';
+import {Link} from "react-router-dom";
+import "./animals.css";
 
 const Animals = ({ animal = '' }) => {
     const [ data, setData ] = useState([]);
@@ -7,24 +9,25 @@ const Animals = ({ animal = '' }) => {
     useEffect(() => {
         // need to change out this url depending on how the backend is built
         
-        fetch('http://localhost:3001/api/animals/')
+        fetch('https://temptails.onrender.com/api/animals/')
             .then(res => res.json())
             .then(json => setData(json));
     }, [])
 
     return (
-        <div className="d-flex flex-wrap justify-content-space-between">
-            {data && data.map(({ id, name, breed, gender, picture01 }) => (
-                <a key={id} className="card" href={`/animal/${id}`}>
-                    <div><img alt="Picture01" src={picture01} /></div>
-                    <p>Name: {name}</p>
-                    <p>Gender: {gender}</p>
-                    <p>Breed: {breed}</p>
-                </a>
+        <div className="pet-grid">
+            {data && data.map((pet, index) => (
+                <Link to={`/animal/${pet.id}`} key={index} className="pet-card">
+                    <img src={pet.picture01} alt={pet.picture01} className="pet-image" />
+                    <div className="pet-info">
+                        <h3>{pet.name}</h3>
+                        <p>{pet.gender}</p>
+                        <p>{pet.breed}</p>
+                    </div>
+                </Link>
             ))}
         </div>
     )
 }
 
 export default Animals;
-
